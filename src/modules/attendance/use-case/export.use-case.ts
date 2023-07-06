d57a17a2e9a50b83c43e4b21af96e354cde83007
Muhamad Habibi Azmi <m.habibiazmi@gmail.com>
2023-07-06 17:36:07 +0700

    feature: export

--- src/modules/attendance/use-case/export.use-case.ts
+++ src/modules/attendance/use-case/export.use-case.ts
@@ -1,4 +1,4 @@
-// import Excel from 'exceljs';
+import Excel from 'exceljs';
 import path from 'path';
 import { ExportAttendanceRepository } from "../model/repository/export.repository.js";
 import DatabaseConnection, { QueryInterface, RetrieveAllOptionsInterface } from "@src/database/connection.js";
@@ -14,37 +14,37 @@ export class ExportAttendanceUseCase {
     try {
       const response = await new ExportAttendanceRepository(this.db).handle(query, options);
 
-      // const workbook = new Excel.Workbook();
-      // const worksheet = workbook.addWorksheet("Attendance");
+      const workbook = new Excel.Workbook();
+      const worksheet = workbook.addWorksheet("Attendance");
 
-      // worksheet.columns = [
-      //   { header: 'Group', key: 'group', width: 18 },
-      //   { header: 'Photo', key: 'photo', width: 13 },
-      //   { header: 'Location', key: 'location', width: 13 },
-      //   { header: 'Email', key: 'email', width: 13 },
-      //   { header: 'Group Name', key: 'groupName', width: 17 }
-      // ];
+      worksheet.columns = [
+        { header: 'Group', key: 'group', width: 18 },
+        { header: 'Photo', key: 'photo', width: 13 },
+        { header: 'Location', key: 'location', width: 13 },
+        { header: 'Email', key: 'email', width: 13 },
+        { header: 'Group Name', key: 'groupName', width: 17 }
+      ];
 
-      // worksheet.columns.forEach((sheetColumn) => {
-      //   sheetColumn.font = {
-      //     size: 12,
-      //   };
-      //   sheetColumn.width = 30;
-      // });
+      worksheet.columns.forEach((sheetColumn) => {
+        sheetColumn.font = {
+          size: 12,
+        };
+        sheetColumn.width = 30;
+      });
       
-      // worksheet.getRow(1).font = {
-      //   bold: true,
-      //   size: 13,
-      // };
+      worksheet.getRow(1).font = {
+        bold: true,
+        size: 13,
+      };
 
-      // response.data.forEach((attendance) => {
-      //   worksheet.addRow(attendance)
-      // });
+      response.data.forEach((attendance) => {
+        worksheet.addRow(attendance)
+      });
 
       const fileName = "attendances-report-" + Math.random().toString(36).substr(2) + ".xlsx";
-      // const exportPath = path.resolve(new URL(import.meta.url).pathname, `../../../../../src/assets/${fileName}`);
+      const exportPath = path.resolve(new URL(import.meta.url).pathname, `../../../../../src/assets/${fileName}`);
 
-      // await workbook.xlsx.writeFile(exportPath);
+      await workbook.xlsx.writeFile(exportPath);
 
       return {
         downloadLink: `http://localhost:3000/assets/${fileName}`,
