d8bb8039afdea69e658c4d1287339b613da8cb40
Muhamad Habibi Azmi <m.habibiazmi@gmail.com>
2023-07-07 10:05:57 +0700

    feature: attendance completed

--- src/modules/attendance/model/repository/retrieve-all.repository.ts
+++ src/modules/attendance/model/repository/retrieve-all.repository.ts
@@ -1,6 +1,7 @@
 import { AttendanceEntityInterface } from "../attendance.entity.js";
 import DatabaseConnection, { QueryInterface, RetrieveAllOptionsInterface } from "@src/database/connection.js";
 import DatabaseManager from "@src/database/database-manager.js";
+import { ObjectId } from "mongodb";
 
 interface DataInterface extends AttendanceEntityInterface {
   _id: string;
@@ -24,6 +25,24 @@ export class RetrieveAllAttendanceRepository {
   }
 
   public async handle(query: QueryInterface, options?: RetrieveAllOptionsInterface): Promise<ResponseInterface> {
-    return await this.databaseManager.retrieveAll(query, options);
+    const aggregates: any = [];
+
+    if (query.filter) {
+      const { date_from, date_to, ...newFilter } = query.filter ?? { date_from: undefined, date_to: undefined };
+
+      if(date_from && date_to) {
+        aggregates.push({ 
+          $match: { 
+            createdAt: { $gte: new Date(new Date(date_from.toString()).toISOString()), $lte: new Date(new Date(date_to.toString()).toISOString()) } 
+          } 
+        })
+      }
+
+      for (const key of Object.keys(newFilter)) {
+        aggregates.push({ $match: { [key]: query.filter[key] } })
+      }
+    }
+
+    return await this.databaseManager.aggregate(aggregates, { page: query.page || 1, pageSize: query.pageSize || 10 });
   }
 }
