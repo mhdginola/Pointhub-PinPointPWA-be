795c8abc0a4cbada2d5db33e8468b030f0f9f277
Muhamad Habibi Azmi <m.habibiazmi@gmail.com>
2023-07-13 11:16:38 +0700

    clean code & add validation unique group

--- src/modules/group/model/repository/update.repository.ts
+++ src/modules/group/model/repository/update.repository.ts
@@ -1,9 +1,11 @@
+import { ApiError } from "@point-hub/express-error-handler";
 import DatabaseConnection, {
   DocumentInterface,
   UpdateOptionsInterface,
   UpdateResultInterface,
 } from "@src/database/connection.js";
 import DatabaseManager from "@src/database/database-manager.js";
+import { ObjectId } from "mongodb";
 
 export class UpdateGroupRepository {
   public databaseManager;
@@ -17,6 +19,18 @@ export class UpdateGroupRepository {
     document: DocumentInterface,
     options?: UpdateOptionsInterface
   ): Promise<UpdateResultInterface> {
+    const { data: isExist } = await this.databaseManager.aggregate([
+      { $match: {
+          _id: { $ne: new ObjectId(id) },
+          name: { $regex: `^${document.name}$`, $options: 'i' }
+        }
+      }
+    ], { page: 1, pageSize: 10 });
+
+    if(isExist.length > 0) {
+      throw new ApiError(422, { name: ["name must be unique"] })
+    }
+
     return await this.databaseManager.update(id, document, options);
   }
 }
