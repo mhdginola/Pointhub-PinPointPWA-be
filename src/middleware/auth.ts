3eec9cff8d452300370c3c14c4cebbc22c64fdb7
Ginola <mhdginola@gmail.com>
2023-07-24 06:26:32 +0700

    addjustment login method

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,10 +1,9 @@
-
+import { ApiError } from "@point-hub/express-error-handler";
+import { Request, Response, NextFunction } from "express";
 import { secretKey } from "../config/auth.js";
-import { Request, Response, NextFunction } from 'express';
-import { ApiError } from '@point-hub/express-error-handler';
-import {  verifyToken } from "../utils/jwt.js";
+import { verifyToken } from "../utils/jwt.js";
 import { retrieve } from "@src/test/utils.js";
- 
+
 async function auth(req: Request, res: Response, next: NextFunction) {
   try {
     const authorizationHeader = req.headers.authorization ?? "";
@@ -16,19 +15,19 @@ async function auth(req: Request, res: Response, next: NextFunction) {
     const authorization: any = verifyToken(authorizationHeader.split(" ")[1], secretKey);
 
     const user = await retrieve("users", authorization.sub);
-    
-    if(!user) {
+
+    if (!user) {
       throw new ApiError(401);
     }
 
-    if(!user?.role) {
+    if (!user?.role) {
       throw new ApiError(403);
     }
- 
+
     next();
   } catch (error) {
     next(error);
   }
 }
 
-export default auth
+export default auth;
--- src/modules/googleApi/googleApi.ts
+++ src/modules/googleApi/googleApi.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
-import passport from "passport";
+import passport, { use } from "passport";
+import { CreateUserRepository } from "../user/model/repository/create.repository.js";
+import { RetrieveAllUserRepository } from "../user/model/repository/retrieve-all.repository.js";
 import { issuer, secretKey } from "@src/config/auth.js";
+import { db } from "@src/database/database.js";
 import { signNewToken } from "@src/utils/jwt.js";
 
 interface UserNew {
+  [x: string]: any;
   id?: string;
   email?: string;
 }
@@ -15,14 +19,37 @@ export const loginWithGoogleCallback = passport.authenticate("google", {
   successRedirect: "/v1/auth/success",
 });
 
-export const loginWithGoogleSuccess = (req: Request, res: Response) => {
+export const loginWithGoogleSuccess = async (req: Request, res: Response) => {
   if (req.isAuthenticated()) {
     const user: UserNew = req.user;
     if (user.id) {
+      // upsert users
       const id: string = user.id;
-      const accessToken = signNewToken(issuer, secretKey, id);
-      res.status(200).json({ accessToken: accessToken });
+      const session = db.startSession();
+      const response = await new RetrieveAllUserRepository(db).handle(
+        {
+          filter: { google_id: id },
+          fields: "",
+          page: 0,
+          pageSize: 0,
+          sort: "",
+        },
+        { session }
+      );
+      if (response.data.length === 0) {
+        const userRepository = new CreateUserRepository(db);
+        const response = await userRepository.handle(
+          { email: user.emails[0].value, username: user.emails[0].value, google_id: id, role: "admin" },
+          { session }
+        );
+        const accessToken = signNewToken(issuer, secretKey, response._id);
+        res.status(200).json({ accessToken: accessToken });
+      } else {
+        const accessToken = signNewToken(issuer, secretKey, response.data[0]._id);
+        res.status(200).json({ accessToken: accessToken });
+      }
     }
+  } else {
+    res.json({ isLoggedin: false });
   }
-  res.json({ isLoggedin: false });
 };
