854b88a43ec91d80245ce27562c6bbd702e5b4e4
Muhamad Habibi Azmi <m.habibiazmi@gmail.com>
2023-08-01 14:35:43 +0700

    fix: callback sso

--- src/config/googleAuth.ts
+++ src/config/googleAuth.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import passport from "passport";
 import { Strategy as GoogleStrategy, VerifyCallback } from "passport-google-oauth20";
+import appConfig from "./app";
 
 dotenv.config();
 type User = {
@@ -13,7 +14,7 @@ passport.use(
     {
       clientID: "160801539479-147jshv55s8esq0ps42mnpu3l61trmk1.apps.googleusercontent.com",
       clientSecret: "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",
-      callbackURL: "http://localhost:3000/v1/auth/google/callback",
+      callbackURL: `${appConfig.url}/v1/auth/google/callback`,
     },
     (accessToken: string, refreshToken: string, profile, done: VerifyCallback) => {
       // Callback setelah login berhasil
